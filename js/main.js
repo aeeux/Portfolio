30ef36eefa696d5a27b7b38fcb5ebcf24a93c0d4
AeeUX <andreaseriksen99@hotmail.com>
2020-12-11 11:31:52 +0100

    update
    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*===== MENU SHOW =====*/ 
+/* Menu Click Event */ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -11,15 +11,15 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*===== ACTIVE AND REMOVE MENU =====*/
+/* Open-Close Menu */
 const navLink = document.querySelectorAll('.nav__link');   
 
 function linkAction(){
-  /*Active link*/
+  /* Open Menu */
   navLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active');
   
-  /*Remove menu mobile*/
+  /* Close menu */
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
 }
